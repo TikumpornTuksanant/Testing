f042821be2dce1e41eb5e9b8ab01ecc47aefd174
TikumpornTuksanant <56583565+TikumpornTuksanant@users.noreply.github.com>
2019-10-18 20:50:28 +0700

    5.3RefactoringTowardsALoosely-coupledDesign

---
+++ TestNinja/UnitTest/TestFundamentals/FakeFileReader.cs
@@ -0,0 +1,12 @@
+﻿using TestNinja.Fundamentals;
+
+namespace UnitTest.TestFundamentals
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
