8c628df42f204695b1ba5814e4112b68f07b70cb
TikumpornTuksanant <56583565+TikumpornTuksanant@users.noreply.github.com>
2019-10-18 17:46:58 +0700

    1.9RefactoringWithConfldence

--- TestNinja/TestNinja/Fundamentals/Reservation.cs
+++ TestNinja/TestNinja/Fundamentals/Reservation.cs
@@ -12,12 +12,7 @@ namespace TestNinja.Fundamentals
 
         public bool CanBeCancelledBy(User user)
         {
-            if (user.IsAdmin)
-                return true;
-            if (MadeBy == user)
-                return true;
-
-            return false;
+            return (user.IsAdmin || MadeBy == user);
         }
 
     }
@@ -26,5 +21,6 @@ namespace TestNinja.Fundamentals
     {
         public bool IsAdmin { get; set; }
     }
+
 
 }
